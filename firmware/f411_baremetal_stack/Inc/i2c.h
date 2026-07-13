486418cc7e40d6d55c3f676f9fe8a42ff2e2af8a
Sohan <jenasohan@gmail.com>
2026-07-12 21:19:58 -0400

    finished i2c driver. test next with sensor address (who_am_i register init)

--- firmware/f411_baremetal_stack/Inc/i2c.h
+++ firmware/f411_baremetal_stack/Inc/i2c.h
@@ -13,6 +13,7 @@
 void I2C_INIT(void);
 /* Read from register */
 uint8_t I2C_Read(uint8_t deviceAddress, uint8_t registerAddress);
-
+/* Write to register */
+void I2C_Write(uint8_t deviceAddress, uint8_t registerAddress, uint8_t data);
 
 #endif /* I2C_H_ */
--- firmware/f411_baremetal_stack/Src/i2c.c
+++ firmware/f411_baremetal_stack/Src/i2c.c
@@ -19,8 +19,16 @@
 #define GPIOB_PUPDR (*(volatile uint32_t*)0x4002040C)
 #define GPIOx_AFRH (*(volatile uint32_t*)0x40020024)
 
-
 /* I2C1 Configuration */
+#define I2C1_CR1 (*(volatile uint32_t*)0x40005400)
+#define I2C1_CR2 (*(volatile uint32_t*)0x40005404)
+#define I2C1_CCR (*(volatile uint32_t*)0x4000541C)
+#define I2C1_TRISE (*(volatile uint32_t*)0x40005420)
+#define I2C1_SR1 (*(volatile uint32_t*)0x40005414)
+#define I2C1_DR (*(volatile uint32_t*)0x40005410)
+#define I2C1_SR2 (*(volatile uint32_t*)0x40005418)
+
+
 
 
 
@@ -41,6 +49,80 @@ void I2C1_INIT(void) {
 
 	GPIOx_AFRH &= ~((15 << 0) | (15 << 4)); // set both pins to be Alternate Function 04, for I2C1 clock/data lines
 	GPIOx_AFRH |= ((4 << 0) | (4 << 4));
+
+	/* Initialize I2C Engine*/
+	I2C_CR1 |= (1 << 15);
+	I2C_CR1 &= ~(1 << 15); // software reset the I2C
+
+	I2C_CR1 = (1 << 4); // set to 16 MHz
+
+	// 100 KHz bus means 10 microsecond period. High time is .5(10) = 5 microseconds since duty cycle is 50/50.
+	// peripheral clock period is 16 MHz which means 0.0625 microsecond period. CCR value = high time / peripheral period = 5 microseconds / 0.0625 microseconds = 80.
+	I2C1_CCR = 80;
+
+	// TRISE = (Max rise time / peripheral clock period) + 1 = (1 microsecond / 0.0625 microsecond) = 17.
+	I2C_TRISE = 17;
+
+	/* Set enable bit */
+	I2C_CR1 |= (1 << 0);
+
+
+uint8_t I2C_Read(uint8_t deviceAddr, uint8_t regAddr) {
+	volatile uint32_t temp; // to clear flags, temp must be used as a dump.
+
+	I2C_CR1 |= (1 << 8); // Start Condition
+	while (!(I2C1_SR1 & (1 << 0))); // set when start condition is sent
+
+	I2C1_DR = deviceAddr; // push sensor address onto DR and send
+
+	while (!(I2C_SR1 & (1 << 1)));
+	temp = I2C1_SR1;
+	temp = I2C1_SR2;
+
+	I2C1_DR = regAddr;
+	while (!(I2C1_SR1 & (1 << 7)));
+
+	I2C1_CR1 |= (1 << 8);
+	while (!(I2C1_SR1 & (1 << 0)));
+
+	I2C1_DR = deviceAddr | 1; // set to read
+	while (!(I2C1_SR1 & (1 << 1)));
+
+	I2C1_CR1 &= ~(1 << 10); // ACK enable
+	temp = I2C1_SR1;
+	temp = I2C1_SR2;
+
+	I2C1_CR1 |= (1 << 9); // Stop
+	while (!(I2C1_SR1 & (1 << 6))); // wait for data to transmit
+
+	return (uint8_t)I2C1_DR;
+}
+
+
+void I2C_Write(uint8_t deviceAddr, uint8_t regAddr, uint8_t data) {
+	volatile uint32_t temp; // to clear flags, temp must be used as dump.
+	// follow same setup as Read for deviceAddr and regAddr
+	I2C_CR1 |= (1 << 8); /
+	while (!(I2C1_SR1 & (1 << 0)));
+
+	I2C1_DR = deviceAddr;
+
+	while (!(I2C_SR1 & (1 << 1)));
+	temp = I2C1_SR1;
+	temp = I2C1_SR2;
+
+	I2C1_DR = regAddr;
+	while (!(I2C1_SR1 & (1 << 7)));
+
+	I2C1_DR = data; // send data on DR
+	while (!(I2C1_SR1 & (1 << 2))); // wait for byte transfer
+
+	I2C1_CR1 |= (1 << 9); // stop condition
+
+}
+
+
+
 
 
 
