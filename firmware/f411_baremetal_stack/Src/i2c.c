55afd83842b0cc6c139b793c9269198b1f734603
Sohan <jenasohan@gmail.com>
2026-07-14 01:15:05 -0400

    complete, refigured i2c driver. Now complete and ready. Next is sensor driver development.

--- firmware/f411_baremetal_stack/Src/i2c.c
+++ firmware/f411_baremetal_stack/Src/i2c.c
@@ -1,130 +1,126 @@
 /*
- * i2c.c
+ * practice.c
  *
- *  Created on: Jul 11, 2026
+ *  Created on: Jul 13, 2026
  *      Author: jenas
  */
 
-#include "i2c.h"
+#include <stdint.h>
 
-/* Memory Registers */
-
-/* Buses */
-#define RCC_APB1ENR (*(volatile uint32_t*)0x40023840)
+/* Configure buses via RCC*/
 #define RCC_AHB1ENR (*(volatile uint32_t*)0x40023830)
+#define RCC_APB1ENR (*(volatile uint32_t*)0x40023840)
 
-/* GPIO */
+/* Memory Mapped I/O Registers */
+
+// need PB8, PB9 from port B
 #define GPIOB_MODER (*(volatile uint32_t*)0x40020400)
 #define GPIOB_OTYPER (*(volatile uint32_t*)0x40020404)
 #define GPIOB_PUPDR (*(volatile uint32_t*)0x4002040C)
-#define GPIOx_AFRH (*(volatile uint32_t*)0x40020024)
+#define GPIOB_AFRH (*(volatile uint32_t*)0x40020424)
 
-/* I2C1 Configuration */
-#define I2C1_CR1 (*(volatile uint32_t*)0x40005400)
-#define I2C1_CR2 (*(volatile uint32_t*)0x40005404)
-#define I2C1_CCR (*(volatile uint32_t*)0x4000541C)
-#define I2C1_TRISE (*(volatile uint32_t*)0x40005420)
-#define I2C1_SR1 (*(volatile uint32_t*)0x40005414)
-#define I2C1_DR (*(volatile uint32_t*)0x40005410)
-#define I2C1_SR2 (*(volatile uint32_t*)0x40005418)
+/* Configure I2C defines */
+// 0x4000 5400 base
+#define I2C_CR1 (*(volatile uint32_t*)0x40005400) // ACK
+#define I2C_CR2 (*(volatile uint32_t*)0x40005404) // FREQ
+#define I2C_DR (*(volatile uint32_t*)0x40005410) // addresses + data sent on
+#define I2C_SR1 (*(volatile uint32_t*)0x40005414)
+#define I2C_SR2 (*(volatile uint32_t*)0x40005418)
+#define I2C_CCR (*(volatile uint32_t*)0x4000541C)
+#define I2C_TRISE (*(volatile uint32_t*)0x40005420)
+
+void I2C_INIT(void) {
+
+	RCC_AHB1ENR |= (1 << 1); // set PortB clock enable
+	RCC_APB1ENR |= (1 << 21); // set I2C1 clock enable
 
 
+	GPIOB_MODER &= ~(3 << 16); // clear MODER8
+	GPIOB_MODER |= (2 << 16); // set MODER8 to alternate function
 
+	GPIOB_MODER &= ~(3 << 18); //clear MODER 8
+	GPIOB_MODER |= (2 << 18);  //set MODER 8 to alternate function
 
+	GPIOB_OTYPER &= ~(1 << 8); // clear type bit 8 (reset)
+	GPIOB_OTYPER |= (1 << 8); // set type bit 8 to open-drain
 
-void I2C1_INIT(void) {
-	/* Initialize buses */
-	RCC_APB1ENR |= (1 << 21);
-	RCC_AHB1ENR |= (1 << 1);
+	GPIOB_OTYPER &= ~(1 << 9); // clear type bit 9 (reset)
+	GPIOB_OTYPER |= (1 << 9); // set type bit 9 to open-drain
 
-	/* Initialize GPIO */
-	// PB8 = SCL, PB9 = SDA
-	GPIOB_MODER &= ~((3 << 16) | (3 << 18)); // clear pins 8/9
-	GPIOB_MODER |= ~((2 << 16) | (2 << 18)); // set pins 8/9 for alternate function
+	GPIOB_PUPDR &= ~(3 << 16); // clear PUPDR8
+	GPIOB_PUPDR |= (1 << 16); // set PUPDR8 to pull up
 
-	GPIOB_OTYPER |= ((1 << 8) | (1 << 9)); // type is open-drain
+	GPIOB_PUPDR &= ~(3 << 18); // clear PUPDR9
+	GPIOB_PUPDR |= (1 << 18); // set PUPDR9 to pull up
 
-	GPIOB_PUPDR &= ~((3 << 16) | (3 << 18)); // set both pins to be pull-up
-	GPIOB_PUPDR &= ~((1 << 16) | (1 << 18));
+	GPIOB_AFRH &= ~(15 << 0); // clear AFRH8
+	GPIOB_ARFH |= (4 << 0);	  // set pin 8 to SCL
 
-	GPIOx_AFRH &= ~((15 << 0) | (15 << 4)); // set both pins to be Alternate Function 04, for I2C1 clock/data lines
-	GPIOx_AFRH |= ((4 << 0) | (4 << 4));
+	GPIOB_AFRH &= ~(15 << 4); // clear AFRH9
+	GPIOB_AFRH |= (4 << 4);   // set pin 9 to SDA
 
-	/* Initialize I2C Engine*/
+	/* I2C Configuration */
+	// start i2c engine
 	I2C_CR1 |= (1 << 15);
-	I2C_CR1 &= ~(1 << 15); // software reset the I2C
+	I2C_CR1 &= ~(1 << 15);
 
-	I2C_CR1 = (1 << 4); // set to 16 MHz
+	I2C_CR2 |= 16; // set 16Mhz for peripheral clock bus
 
-	// 100 KHz bus means 10 microsecond period. High time is .5(10) = 5 microseconds since duty cycle is 50/50.
-	// peripheral clock period is 16 MHz which means 0.0625 microsecond period. CCR value = high time / peripheral period = 5 microseconds / 0.0625 microseconds = 80.
-	I2C1_CCR = 80;
+	I2C_CCR = 80; // calculated from 100KHz high time of 5 microseconds divided by period of peripheral clock bus. 5 / 0.0625 = 80
+	I2C_TRISE = 17; // calculated from 100Khz max low-to-high time which is 1ns / 0.0625(period of peripheral clock bus) = 17
 
-	// TRISE = (Max rise time / peripheral clock period) + 1 = (1 microsecond / 0.0625 microsecond) = 17.
-	I2C_TRISE = 17;
-
-	/* Set enable bit */
-	I2C_CR1 |= (1 << 0);
-
-
-uint8_t I2C_Read(uint8_t deviceAddr, uint8_t regAddr) {
-	volatile uint32_t temp; // to clear flags, temp must be used as a dump.
-
-	I2C_CR1 |= (1 << 8); // Start Condition
-	while (!(I2C1_SR1 & (1 << 0))); // set when start condition is sent
-
-	I2C1_DR = deviceAddr; // push sensor address onto DR and send
-
-	while (!(I2C_SR1 & (1 << 1)));
-	temp = I2C1_SR1;
-	temp = I2C1_SR2;
-
-	I2C1_DR = regAddr;
-	while (!(I2C1_SR1 & (1 << 7)));
-
-	I2C1_CR1 |= (1 << 8);
-	while (!(I2C1_SR1 & (1 << 0)));
-
-	I2C1_DR = deviceAddr | 1; // set to read
-	while (!(I2C1_SR1 & (1 << 1)));
-
-	I2C1_CR1 &= ~(1 << 10); // ACK enable
-	temp = I2C1_SR1;
-	temp = I2C1_SR2;
-
-	I2C1_CR1 |= (1 << 9); // Stop
-	while (!(I2C1_SR1 & (1 << 6))); // wait for data to transmit
-
-	return (uint8_t)I2C1_DR;
-}
-
-
-void I2C_Write(uint8_t deviceAddr, uint8_t regAddr, uint8_t data) {
-	volatile uint32_t temp; // to clear flags, temp must be used as dump.
-	// follow same setup as Read for deviceAddr and regAddr
-	I2C_CR1 |= (1 << 8); /
-	while (!(I2C1_SR1 & (1 << 0)));
-
-	I2C1_DR = deviceAddr;
-
-	while (!(I2C_SR1 & (1 << 1)));
-	temp = I2C1_SR1;
-	temp = I2C1_SR2;
-
-	I2C1_DR = regAddr;
-	while (!(I2C1_SR1 & (1 << 7)));
-
-	I2C1_DR = data; // send data on DR
-	while (!(I2C1_SR1 & (1 << 2))); // wait for byte transfer
-
-	I2C1_CR1 |= (1 << 9); // stop condition
+	I2C_CR1 |= (1 << 0); // enable peripheral
 
 }
 
 
+void I2C_WRITE(uint8_t deviceAddr, uint8_t regAddr, uint8_t data) {
+	volatile uint32_t temp;
 
+	I2C_CR1 |= (1 << 8); // start generation..pull SDA line low while SCL is high
+	while(!(I2C_SR1 & (1 << 0))); // wait until hardware knows bus is transitioned
 
+	I2C_DR = (deviceAddr << 1); // the LSB must be 0 since writing to device
+	while(!(I2C_SR1 & (1 << 1))); // wait until address is received and ACK is sent!
+	temp = I2C_SR1;
+	temp = I2C_SR2; // complete clearing sequence after previous step as told in reference manual
 
+	I2C_DR = regAddr;
+	while (!(I2C_SR1 & (1 << 7))); // wait for TxE
+	// code up to here is used in both I2C_WRITE and I2C_READ
 
+	I2C_DR = data;
+	while (!(I2C_SR1 & (1 << 2))); // wait for byte transfer
 
+	I2C_CR1 |= (1 << 9); // set stop condition
+}
+
+uint8_t I2C_READ(uint8 deviceAddr, uint8_t regAddr) {
+	volatile uint32_t temp;
+
+	I2C_CR1 |= (1 << 8); // start generation..pull SDA line low while SCL is high
+	while(!(I2C_SR1 & (1 << 0))); // wait until hardware knows bus is transitioned
+
+	I2C_DR = (deviceAddr << 1); // the LSB must be 0 since writing to device
+	while(!(I2C_SR1 & (1 << 1))); // wait until address is received and ACK is sent!
+	temp = I2C_SR1;
+	temp = I2C_SR2; // complete clearing sequence after previous step as told in reference manual
+
+	I2C_DR = regAddr;
+	while (!(I2C_SR1 & (1 << 2))); // wait for BTF in READ
+
+	I2C_CR1 |= (1 << 8); // Start Condition again
+	while (!(I2C_SR1 & (1 << 0))); // same seq
+
+	I2C1_DR = (deviceAddr << 1) | 1; // set to read
+	while (!(I2C_SR1 & (1 << 1))); // wait till ACK
+
+	I2C_CR1 &= ~(1 << 10); // ACK enable
+	temp = I2C_SR1;
+	temp = I2C_SR2;
+
+	I2C_CR1 |= (1 << 9); // Stop
+	while (!(I2C_SR1 & (1 << 6))); // wait for data to transmit
+
+	return (uint8_t)I2C_DR;
 }
